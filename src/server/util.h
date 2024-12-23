60592ed87e03bbba268ffe8f88690719ed9c5ec9
Gabriele Bartolomei <g.bartolomei6@studenti.unipi.it>
2024-12-23 16:29:18 +0100

    Commenti in italiano util.h

--- src/server/util.h
+++ src/server/util.h
@@ -1,8 +1,8 @@
 #include <stdbool.h>
 
-/// @brief Returns the parent directory of a path
-/// @param  path The string containing the path
-/// @return true if succeded
+/// @brief Ottiene la directory padre
+/// @param  path La stringa contentente il percorso
+/// @return true in caso di modifica
 bool parentDirectory(char * path);
 
 /// @brief Rimuove i primi caratteri non alfabetici da una stringa
@@ -10,16 +10,17 @@ bool parentDirectory(char * path);
 /// @return true in caso di modifica
 bool removeNumbering(char * string);
 
-/// @brief Returns the path, with the filename and not the extension
-/// @param path The string containing the path
-/// @return true if succeded
+/// @brief Rimuove l'estensione dal nome del file
+/// @param path La stringa contenente il nome del file
+/// @return true in caso di modifica
 bool removeExtension(char * path);
 
-/// @brief Returns the location of the executable file
-/// @param string String where to write if NULL it will be allocated dynamically
-/// @return The string containing the executable path
+/// @brief Ottiene il percorso dell'eseguibile del file
+/// @param string Stringa in cui scrivere il persorso, se NULL alloca dinamicamente
+/// @return Il puntatore alla stringa contenente il percorso dell'eseguibile
 char * executablePath(char * string);
 
-/// @brief Replace the newline character '\\n' with endline character '\\0'
-/// @param path String containing '\n' character
+/// @brief Sostituisce il carattere di nuova linea '\\n' con quello di termine stringa '\\0'
+/// @param path Stringa contenente il carattere di nuova linea'\n' 
+/// @return true in caso di modifica
 bool newlineReplace(char * path);
